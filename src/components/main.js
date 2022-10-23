0aa47d83fc4be26e23c923e47d4b113a035f1d15
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-10-23 16:29:46 -0500

    fixing svg import

--- src/components/main.js
+++ src/components/main.js
@@ -1,9 +1,9 @@
-import samImg from "../assets/sam.svg";
+import SamPicture from "../assets/sam.svg";
 
 const Main = () => {
   return (
     <main className="main-content">
-      <img src={samImg} alt="samImage" />
+      <SamPicture />
     </main>
   );
 };
