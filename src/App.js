09f508c5d7a4f0f8d3c45cdf16bb935fa2e51592
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-10-23 18:38:24 -0500

    building up the ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import Nav from "./components/nav";
 import "./App.css";
+import Main from "./components/main";
 
 function App() {
   return (
     <Fragment>
       <Nav />
+      <Main />
     </Fragment>
   );
 }
