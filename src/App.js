60e971c1aa5386deef78801e42a4238f7e3017b5
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-10-24 22:18:21 -0500

    workin on the ip page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import IpAddress from "./pages/ipaddress";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="container">
         <Routes>
           <Route index element={<Home />} />
+          <Route path="/ipaddress" element={<IpAddress />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/ipaddress.js
@@ -0,0 +1,14 @@
+import Nav from "../components/nav";
+
+const IpAddress = () => {
+  return (
+    <div className="container">
+      <Nav />
+      <main className="main">
+        <h1>Your IP Address is</h1>
+      </main>
+    </div>
+  );
+};
+
+export default IpAddress;
