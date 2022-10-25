13bf18fe82a540f914609209f4fcb4115d3a49a5
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-10-24 22:35:31 -0500

    adding enviroment variables to project

--- src/pages/ipaddress.js
+++ src/pages/ipaddress.js
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Nav from "../components/nav";
 import "../styles/ipaddress.css";
 
 const IpAddress = () => {
   const [ipaddress, setIp] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.ipify.org")
+      .then((response) => {
+        console.log(response.json());
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="container">
       <Nav />
